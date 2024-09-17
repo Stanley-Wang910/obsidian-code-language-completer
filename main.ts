94eee82e9dd735f559a620ef9c35208adb292db2
twinfantasyfan <wangstanley910@gmail.com>
2024-09-17 02:43:30 -0400

    init commit!

--- main.ts
+++ main.ts
@@ -1,134 +1,235 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import {
+	App,
+	Editor,
+	editorInfoField,
+	EditorPosition,
+	EditorSuggest,
+	EditorSuggestContext,
+	EditorSuggestTriggerInfo,
+	MarkdownView,
+	Modal,
+	Notice,
+	Plugin,
+	PluginSettingTab,
+	Setting,
+	TFile,
+} from "obsidian";
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface CodeBlockInserterSettings {
+	lastUsedLanguage: string;
+	additionalLanguages: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+const DEFAULT_SETTINGS: CodeBlockInserterSettings = {
+	lastUsedLanguage: "",
+	additionalLanguages: "",
+};
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class CodeBlockInserterPlugin extends Plugin {
+	suggester: LanguageSuggester;
+	settings: CodeBlockInserterSettings;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		//Init and register LanguageSuggester
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+		this.suggester = new LanguageSuggester(this);
+		this.registerEditorSuggest(this.suggester);
 
-		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
+			id: "insert-code-block-custom",
+			name: "Insert Code Block (Custom)",
 			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
+				const cursor = editor.getCursor();
+				editor.replaceRange("```\n\n```", cursor);
+				editor.setCursor({
+					line: cursor.line,
+					ch: cursor.ch + 3,
+				});
+			},
+			hotkeys: [
+				{
+					modifiers: ["Mod", "Shift"],
+					key: "C",
+				},
+			],
 		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		this.addSettingTab(new CodeBlockInserterSettingTab(this.app, this));
 	}
-
-	onunload() {
-
-	}
-
 	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+		this.settings = Object.assign(
+			{},
+			DEFAULT_SETTINGS,
+			await this.loadData(),
+		);
 	}
-
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
+class LanguageSuggester extends EditorSuggest<string> {
+	plugin: CodeBlockInserterPlugin;
+	languages: string[];
+
+	constructor(plugin: CodeBlockInserterPlugin) {
+		super(plugin.app);
+		this.plugin = plugin;
+		this.languages = [];
+		this.updateLanguages();
+	}
+	updateLanguages() {
+		const baseLanguages = [
+			"javascript",
+			"python",
+			"java",
+			"c",
+			"cpp",
+			"csharp",
+			"ruby",
+			"go",
+			"rust",
+			"swift",
+			"kotlin",
+			"php",
+			"html",
+			"css",
+			"sql",
+			"bash",
+			"powershell",
+			"markdown",
+			"json",
+			"yaml",
+			"xml",
+			"typescript",
+			"ocaml",
+		];
+
+		const additionalLanguages = this.plugin.settings.additionalLanguages
+			.split(",")
+			.map((lang) => lang.trim())
+			.filter((lang) => lang.length > 0);
+
+		this.languages = Array.from(
+			new Set([...additionalLanguages, ...baseLanguages]),
+		);
 	}
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
+	onTrigger(
+		cursor: EditorPosition,
+		editor: Editor,
+		file: TFile,
+	): EditorSuggestTriggerInfo | null {
+		const line = editor.getLine(cursor.line);
+		const match = line.match(/```(\w*)$/);
+
+		if (match) {
+			return {
+				start: { line: cursor.line, ch: cursor.ch - match[1].length },
+				end: cursor,
+				query: match[1],
+			};
+		}
+		return null;
 	}
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
+	getSuggestions(
+		context: EditorSuggestContext,
+	): string[] | Promise<string[]> {
+		const query = context.query.toLowerCase();
+		let suggestions = this.languages.filter((lang) =>
+			lang.startsWith(query),
+		);
+		// Prioritize last used language
+
+		const lastUsedLanguage = this.plugin.settings.lastUsedLanguage;
+		if (
+			lastUsedLanguage &&
+			lastUsedLanguage.startsWith(query) &&
+			suggestions.includes(lastUsedLanguage)
+		) {
+			// move to top
+			suggestions = [
+				lastUsedLanguage,
+				...suggestions.filter((lang) => lang !== lastUsedLanguage),
+			];
+		}
+
+		return suggestions;
+	}
+
+	renderSuggestion(lang: string, el: HTMLElement): void {
+		el.setText(lang);
+	}
+
+	selectSuggestion(lang: string, evt: MouseEvent | KeyboardEvent): void {
+		const { editor, start, end } = this.context!;
+		editor.replaceRange(lang, start, end);
+
+		// update last used language
+		this.plugin.settings.lastUsedLanguage = lang;
+		this.plugin.saveSettings();
+
+		// editor.setCursor(end.line, start.ch + lang.length);
+		const newCursorPos = {
+			line: end.line + 1,
+			ch: 0,
+		};
+		editor.setCursor(newCursorPos);
 	}
 }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+class CodeBlockInserterSettingTab extends PluginSettingTab {
+	plugin: CodeBlockInserterPlugin;
 
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: CodeBlockInserterPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
+			.setName("Last Used Language")
+			.setDesc("The last programming language you used in a code block.")
+			.addText((text) =>
+				text
+					.setPlaceholder("No language selected yet")
+					.setValue(this.plugin.settings.lastUsedLanguage)
+					.onChange(async (value) => {
+						this.plugin.settings.lastUsedLanguage = value;
+						await this.plugin.saveSettings();
+					}),
+			);
+		new Setting(containerEl)
+			.setName("Additional Languages")
+			.setDesc("Add more languages (comma-separated).")
+			.addTextArea((text) =>
+				text
+					.setPlaceholder("e.g., ruby, lang1, lang2")
+					.setValue(this.plugin.settings.additionalLanguages)
+					.onChange(async (value) => {
+						this.plugin.settings.additionalLanguages = value;
+						await this.plugin.saveSettings();
+
+						// Update the languages in the suggester
+						this.plugin.suggester.updateLanguages();
+					}),
+			);
+
+		new Setting(containerEl)
+			.setName("Reset Last Used Language")
+			.setDesc("Clear the last used language.")
+			.addButton((button) =>
+				button.setButtonText("Reset").onClick(async () => {
+					this.plugin.settings.lastUsedLanguage = "";
 					await this.plugin.saveSettings();
-				}));
+					this.display();
+				}),
+			);
 	}
 }
